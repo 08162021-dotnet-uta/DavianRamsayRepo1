14f229b48623f4efffc94e476f7a00386247ef36
davian ramsay <davian.ramsay@revature.net>
2021-08-17 19:34:45 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# davian
+
+##
+
+![] (https://avatars.githubusercontent.com/u/89043364?v=4&s=64)
+
+
+## License
+
+[MIT License] (https://)
+
---
+++ week_1/HelloCsharp/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace HelloCsharp
+{
+  internal class NewBaseType
+  {
+
+  }
+
+  class Program : NewBaseType
+  {
+
+    int add(int a, int b)
+    {
+
+
+      return (a + b);
+    }
+
+    int subtract(int b, int c)
+    {
+      return (b - c);
+    }
+
+    int multiply(int d, int e)
+    {
+      return (d - e);
+    }
+
+    int divide(int f, int g)
+    {
+
+      return (f / g);
+    }
+
+
+    // Build a calculator using 5 methods: add, multiply, subtract, divide, print 
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+    }
+
+  }
+
+
+
+
+}
+
+
