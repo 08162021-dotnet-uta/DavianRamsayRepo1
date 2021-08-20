95b52fb11d44362cceb07b348e7f9d1843d66dcc
davian ramsay <davian.ramsay@revature.net>
2021-08-20 01:41:28 -0400

    davian_repo

---
+++ projects/project_0/Project0.StoreApplication.Domain/Models/Store.cs
@@ -0,0 +1,13 @@
+namespace Project0.StoreApplication.Domain.Models
+{
+
+  public class Store
+  {
+    public string Name { get; set; }
+    public override string ToString()
+    {
+      return Name;
+    }
+
+  }
+}
